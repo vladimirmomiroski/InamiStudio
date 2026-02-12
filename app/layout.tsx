73fd79519983ad71f0efce5bdee3ea0b9acbb6b2
vladimirmomiroski <83420111+vladimirmomiroski@users.noreply.github.com>
2026-02-12 13:40:45 +0100

    basic structure added

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Montserrat, Anton } from "next/font/google";
+import "./globals.css";
+
+const montserrat = Montserrat({
+  variable: "--montserrat",
+  subsets: ["latin"],
+});
+
+const anton = Anton({
+  variable: "--anton",
+  subsets: ["latin"],
+  weight: "400",
+});
+
+export const metadata: Metadata = {
+  title: "Inami Studio",
+  description: "Marketing Agency",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${montserrat.variable} ${anton.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <h1 className="text-extra-large">Marketing Agency</h1>
+       <p className="text-default">Something else</p>
+      </main>
+    </div>
+  );
+}
